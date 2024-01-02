8e1dcf2cfa985d0afa2fa86bc948e7d8fa9d853d
Talfaza <charifyahia4@gmail.com>
2024-01-02 13:03:45 +0100

    new method

--- classes/Product.php
+++ classes/Product.php
@@ -6,10 +6,13 @@ class Product {
     private $price;
     private $quantity;
 
-    public function __construct($name, $price, $quantity) {
+    private $conn;
+
+    public function __construct($name, $price, $quantity,$conn) {
         $this->name = $name;
         $this->price = $price;
         $this->quantity = $quantity;
+        $this->conn = $conn;
     }
 
     public function getId() { return $this->id; }
@@ -38,8 +41,40 @@ class Product {
         }
     }
 
+    public function updateQte($id){
+        $qte_bought = NULL;
+        $query = "SELECT * FROM TEST_I.PRODUCT WHERE id_product = '$id'";
+
+        $result = mysqli_query($this->conn, $query);
+        $row = mysqli_fetch_array($result);
+        $qte = $row['product_quantity_id'];
+
+        if (isset($_POST['submit'])) {
+            $qte_bought =  $_POST['qte_product'];
+        }
+
+        if ($qte_bought > $qte) {
+            $error = '<div class="notification is-primary" style="margin-left:150px;">
+            <button class="delete"></button>
+            Quantity exceeding the stock limit <br><br><b>Wait for a restock</b>
+            </div>';
+            return $error;
+        }
+
+        $new_qte = $qte - $qte_bought;
+
+        $query = "UPDATE TEST_I.PRODUCT SET product_quantity_id = '$new_qte' WHERE id_product = '$id'";
+
+        $result = mysqli_query($this->conn, $query);
+
+
+       
+    }
+
 
 }
+
+
 
 
 
