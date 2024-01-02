9bfb57e7501395ee7c13a07fe9a01eadb7f198fd
Talfaza <charifyahia4@gmail.com>
2024-01-02 21:13:58 +0100

    delete unsed code

--- admin/products/delete.php
+++ admin/products/delete.php
@@ -6,8 +6,7 @@ $conn = (new Connection())->getConnection();
 if(isset($_GET['deleteid'])){
       $id = $_GET['deleteid'];
       $query = "DELETE FROM TEST_I.PRODUCT WHERE id = $id";
-      $query = "DELETE FROM TEST_I.PRICE WHERE id = $id";
-      $query = "DELETE FROM TEST_I.QUANTITY WHERE id = $id";
+
       $result = mysqli_query($conn,$query);
       if($result){
         header("Location: contact.php");
