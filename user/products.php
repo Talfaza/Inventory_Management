605e243d1a0343310ccce6a685a747336f812f17
Talfaza <charifyahia4@gmail.com>
2024-01-04 04:01:41 +0100

    title

--- user/products.php
+++ user/products.php
@@ -65,7 +65,7 @@ if (!($product->updateQte($id) == TRUE )) {$cart->insertCart();}
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Cover - Free Bulma template</title>
+    <title>Inventory Mangement</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
